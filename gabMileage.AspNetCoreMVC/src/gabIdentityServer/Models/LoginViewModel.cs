8d734750f1ae701e77d3f573ec23f44baff5da8d
Greg Brewer <greg@gabrewer.com>
2016-09-13 23:53:58 -0700

    Initial ASP.NET Core MVC with Identity Server checkin

---
+++ gabMileage.AspNetCoreMVC/src/gabIdentityServer/Models/LoginViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace gabIdentityServer.Models
+{
+    public class LoginViewModel : LoginInputModel
+    {
+        public LoginViewModel(HttpContext httpContext)
+        {
+            ExternalProviders = httpContext.Authentication.GetAuthenticationSchemes()
+                .Where(x => x.DisplayName != null)
+                .Select(x => new ExternalProvider
+                {
+                    DisplayName = x.DisplayName,
+                    AuthenticationScheme = x.AuthenticationScheme
+                });
+        }
+
+        public LoginViewModel(HttpContext httpContext, LoginInputModel other)
+            : this(httpContext)
+        {
+            Username = other.Username;
+            Password = other.Password;
+            RememberLogin = other.RememberLogin;
+            ReturnUrl = other.ReturnUrl;
+        }
+
+        public string ErrorMessage { get; set; }
+        public IEnumerable<ExternalProvider> ExternalProviders { get; set; }
+    }
+
+    public class ExternalProvider
+    {
+        public string DisplayName { get; set; }
+        public string AuthenticationScheme { get; set; }
+    }
+}
