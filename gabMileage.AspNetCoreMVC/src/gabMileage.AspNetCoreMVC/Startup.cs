69f0cdf0632d063430dc341b0b82d39e12e805cd
Greg Brewer <greg@gabrewer.com>
2016-09-14 21:52:33 -0700

    Initial authentication fro mvc app

--- gabMileage.AspNetCoreMVC/src/gabMileage.AspNetCoreMVC/Startup.cs
+++ gabMileage.AspNetCoreMVC/src/gabMileage.AspNetCoreMVC/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -34,6 +35,8 @@ namespace gabMileage.AspNetCoreMVC
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
@@ -48,6 +51,33 @@ namespace gabMileage.AspNetCoreMVC
             }
 
             app.UseStaticFiles();
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationScheme = "Cookies",
+                AutomaticAuthenticate = true
+            });
+
+            var oidcOptions = new OpenIdConnectOptions
+            {
+                AuthenticationScheme = "oidc",
+                SignInScheme = "Cookies",
+                Authority = "http://localhost:47627",
+                RequireHttpsMetadata = false,
+                PostLogoutRedirectUri = "http://localhost:47834/",
+                ClientId = "gabmileagemvc",
+                ClientSecret = "diVeTryCOner",
+                ResponseType = "code id_token",
+                GetClaimsFromUserInfoEndpoint = true,
+                SaveTokens = true
+            };
+
+            oidcOptions.Scope.Clear();
+            oidcOptions.Scope.Add("openid");
+            oidcOptions.Scope.Add("profile");
+            oidcOptions.Scope.Add("gabMileageApi");
+
+            app.UseOpenIdConnectAuthentication(oidcOptions);
 
             app.UseMvc(routes =>
             {
