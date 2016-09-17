12b513409414087dbf6683a9aef40384f400fd45
Greg Brewer <greg@gabrewer.com>
2016-09-16 22:45:07 -0700

    initial asp.net identity checkin

---
+++ gabMileage.AspNetCoreMVC/src/gabIdentityServer/Features/Account/Manage/Models/RemoveLoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace gabIdentityServer.Features.Manage
+{
+    public class RemoveLoginViewModel
+    {
+        public string LoginProvider { get; internal set; }
+        public string ProviderKey { get; internal set; }
+    }
+}
---
+++ gabMileage.AspNetCoreMVC/src/gabIdentityServer/Services/MessageServices.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gabIdentityServer.Services
+{
+    public class MessageServices : IEmailSender, ISmsSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            // TODO: implement email service.  for now nop
+            return Task.FromResult(0);
+        }
+
+        public Task SendSmsAsync(string number, string message)
+        {
+            // TODO: implement email service.  for now nop
+            return Task.FromResult(0);
+        }
+    }
+}
