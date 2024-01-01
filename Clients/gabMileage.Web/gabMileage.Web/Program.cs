7afc0e7835dba45821073d1cc6cf336733e33ba0
Greg Brewer <greg@gabrewer.com>
2024-01-01 11:39:44 -0500

    Initial commit moving to .Net Aspire (#1)

---
+++ Clients/gabMileage.Web/gabMileage.Web/Program.cs
@@ -0,0 +1,82 @@
+﻿using gabMileage.Web.Client.Pages;
+using gabMileage.Web.Components;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+builder.AddRedisDistributedCache("cache");
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddDaprClient();
+
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.Unspecified;
+    // Handling SameSite cookie according to https://docs.microsoft.com/en-us/aspnet/core/security/samesite?view=aspnetcore-3.1
+    options.HandleSameSiteCookieCompatibility();
+});
+
+var scopes = builder.Configuration["Mileage:Scopes"];
+ArgumentNullException.ThrowIfNull(scopes);
+builder.Services.AddMicrosoftIdentityWebAppAuthentication(builder.Configuration, Constants.AzureAdB2C)
+    .EnableTokenAcquisitionToCallDownstreamApi(new string[] { scopes })
+    .AddDistributedTokenCaches();
+
+builder.Services.AddAuthorizationBuilder();
+builder.Services.AddCascadingAuthenticationState();
+
+builder.Services.AddControllersWithViews()
+    .AddMicrosoftIdentityUI();
+
+builder.Services.AddRazorPages();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddMicrosoftIdentityConsentHandler()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddServerSideBlazor()
+    .AddMicrosoftIdentityConsentHandler();
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddOptions();
+builder.Services.Configure<OpenIdConnectOptions>(builder.Configuration.GetSection("AzureAdB2C"));
+
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+
+app.UseCookiePolicy();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Dashboard).Assembly);
+
+app.MapControllers();
+
+await app.RunAsync();
--- gabIdentityServer/Features/Account/Manage/Models/RemoveLoginViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace gabIdentityServer.Features.Account
-{
-    public class RemoveLoginViewModel
-    {
-        public string LoginProvider { get; internal set; }
-        public string ProviderKey { get; internal set; }
-    }
-}
