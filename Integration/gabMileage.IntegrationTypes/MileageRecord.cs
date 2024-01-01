6e359b9730468d466e8a0044bf8c700099a334a7
Greg Brewer <greg@gabrewer.com>
2024-01-01 11:25:46 -0500

    Initial commit moving to .Net Aspire

---
+++ Integration/gabMileage.IntegrationTypes/MileageRecord.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace gabMileage.IntegrationTypes;
+
+public class MileageRecord
+{
+    public Guid Id { get; set; }
+    public DateTime FilledAt { get; set; }
+    public double StartingMileage { get; set; }
+    public double EndingMileage { get; set; }
+    public double Gallons { get; set; }
+    public decimal Cost { get; set; }
+    public string Station { get; set; }
+}
