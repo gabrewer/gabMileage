b6b199bf307be666a900af69d916275f90734515
Greg Brewer <greg@gabrewer.com>
2024-01-01 11:39:44 -0500

    Initial commit moving to .Net Aspire (#1)

---
+++ Services/gabMileage.Mileage.Api/Program.cs
@@ -0,0 +1,43 @@
+﻿using Dapr.Client;
+using Dapr.Extensions.Configuration;
+using gabMileage.IntegrationTypes;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+builder.Configuration.AddDaprSecretStore(
+    "gab-secretstore",
+    new DaprClientBuilder().Build());
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/mileagerecords", () =>
+{
+    var mileageRecords = Enumerable.Range(1, 5).Select(index => new MileageRecord
+    {
+        FilledAt = DateTime.Now.AddDays(index),
+
+    }).ToArray();
+
+    return mileageRecords;
+})
+.WithName("GetMileageRecords")
+.WithOpenApi();
+
+app.Run();
+
---
+++ gabMileage.AppHost/Program.cs
@@ -0,0 +1,18 @@
+﻿using Aspire.Hosting.Dapr;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var cache = builder.AddRedis("cache");
+
+var secretStore = builder.AddDaprComponent("gab-secretstore", "secretstores", new
+    DaprComponentOptions { LocalPath = "../dapr/components/" });
+
+var apiService = builder.AddProject<Projects.gabMileage_Mileage_Api>("mileage-api")
+    .WithDaprSidecar()
+    .WithReference(secretStore);
+
+builder.AddProject<Projects.gabMileage_Web>("gabmileage-web")
+    .WithDaprSidecar()
+    .WithReference(cache);
+
+builder.Build().Run();
